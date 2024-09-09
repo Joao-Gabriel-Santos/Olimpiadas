c9af40d2b408d07cbd6e68be1a29ca3144a43144
João Gabriel dos Santos <128163433+Joao-Gabriel-Santos@users.noreply.github.com>
2024-09-09 12:26:19 -0200

    Criação do html, css e inicio de javascript

---
+++ dados.js
@@ -0,0 +1,19 @@
+let  atletas = [
+    {
+        nome: "Rayssa Leal",
+        descricao: `Rayssa Leal, conhecida como "Fadinha", é uma skatista brasileira que, aos 13 anos, se tornou a mais jovem medalhista olímpica da história do Brasil. Com seu estilo único e carisma, ela conquistou o mundo e inspirou uma nova geração de skatistas.`,
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+    },
+    {
+        nome: "Martine Grael",
+        descricao: `Martine Grael é uma velejadora brasileira que, ao lado de Kahena Kunze, conquistou a medalha de ouro na classe 49er FX nos Jogos Olímpicos de Tóquio 2020.`,
+        link: "https://pt.wikipedia.org/wiki/Martine_Grael"
+      },
+      {
+        nome: "Aline Silva",
+        descricao: `Aline Silva é uma judoca brasileira que conquistou a medalha de bronze nos Jogos Olímpicos de Tóquio 2020.`,
+        link: "https://pt.wikipedia.org/wiki/Aline_Silva_(judoca)"
+      },
+    "Rebeca Andrade",
+    "Beatriz Souza"
+]
