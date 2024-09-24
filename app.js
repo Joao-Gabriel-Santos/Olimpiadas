e1fbfda741fd2d9e847bc42a514c3d90189e196e
João Gabriel dos Santos <128163433+Joao-Gabriel-Santos@users.noreply.github.com>
2024-09-24 13:43:31 -0200

    Iniciando o Back-End
    Fazendo a função de pesquisar e ocultar os atletas

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log(dados)
+
+function buscar() {
+    const nome = document.getElementsByTagName('input')[0].value;
+    let res = document.querySelector('.resultados-pesquisa');
+    res.innerHTML = 'CERTO'
+}
